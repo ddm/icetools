457b54656aca7d5754b6044bac81452b2a98dd75
ddm <dimitri.delmarmol@gmail.com>
2016-12-29 14:04:11 +0100

    Fix missing dependency on osx

--- lattice-fpga-toolchain.sh
+++ lattice-fpga-toolchain.sh
@@ -10,7 +10,7 @@ UNAME_STR=`uname`
 
 echo "Installing dependencies..."
 if [[ "$UNAME_STR" == "Darwin" ]] && hash brew 2>/dev/null; then
-	brew update && brew install python3 libftdi0 libffi autoconf bison gawk gnu-sed graphviz xdot
+	brew update && brew install python3 libftdi0 libffi autoconf bison gawk gnu-sed graphviz xdot mercurial
 fi
 if [[ "$UNAME_STR" == "Linux" ]] && hash apt-get 2>/dev/null; then
 	sudo apt-get install -y build-essential clang bison flex libreadline-dev gawk tcl-dev libffi-dev git mercurial graphviz xdot pkg-config python python3 libftdi-dev
